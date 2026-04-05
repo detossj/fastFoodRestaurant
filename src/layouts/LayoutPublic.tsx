91a0a7578ed2afb53f882ae005bd3f28625cd774
detossj <jorgerubilarssj@gmail.com>
2026-04-05 11:38:29 -0400

    Se cambio import de Topbar en LayoutPublic.tsx

--- src/layouts/LayoutPublic.tsx
+++ src/layouts/LayoutPublic.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "@tanstack/react-router"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import Topbar from "../components/TopBar"
+import Topbar from "../components/Topbar"
 
 const LayoutPublic = () => {
 
