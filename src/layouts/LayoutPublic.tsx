bfe025ebec4ca353ab925d23c8e35ad756f87bdf
detossj <jorgerubilarssj@gmail.com>
2026-04-05 11:17:41 -0400

    Se creo LayoutPublic.jsx

---
+++ src/layouts/LayoutPublic.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "@tanstack/react-router"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Topbar from "../components/TopBar"
+
+const LayoutPublic = () => {
+
+  return (
+    <>
+      <Topbar />
+      <Navbar />
+        <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default LayoutPublic
