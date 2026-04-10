c0c365019e9e7f8f2b8363332c6e193ca0aec82d
detossj <jorgerubilarssj@gmail.com>
2026-04-10 14:35:39 -0400

    Se agrego la interface Product y la funcion getProducts en fastFoodRestaurantService.ts

--- src/services/fastFoodRestaurantService.ts
+++ src/services/fastFoodRestaurantService.ts
@@ -13,12 +13,39 @@ export interface Promocion {
   updated_at?: string; 
 }
 
+export interface Product {
+    id: number;
+    name: string;
+    description?: string | null; 
+    price: string | number;
+    image_url?: string | null; 
+    available: boolean;
+    category_id: number;
+    created_at?: string; 
+    updated_at?: string; 
+  }
+
 export const getPromociones = async (): Promise<Promocion[]> => {
     try {
         const response = await api_url.get<Promocion[]>('/promotions');
         return response.data;
     } catch (error) {
         console.error("Error cargando promociones:", error);
+        throw error;
+    }
+};
+
+export const getProducts = async (categoria: string): Promise<Product[]> => {
+    try {
+        // Axios convertirá esto automáticamente en: /products?category_id=X
+        const response = await api_url.get<Product[]>('/products', {
+            params: {
+                category_id: categoria
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error cargando los productos:", error);
         throw error;
     }
 };
