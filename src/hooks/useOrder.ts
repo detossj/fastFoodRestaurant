04672eb3cd6be10da489b5f434b7c06b40e4c7f5
detossj <jorgerubilarssj@gmail.com>
2026-04-20 15:23:56 -0400

    Se agrego la funcion useGetOrders en useOrders.ts

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,13 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
-import { createOrder } from "@/services/fastFoodRestaurantService";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createOrder, getOrders } from "@/services/fastFoodRestaurantService";
 import type { Order } from "@/types/order.types";
 
 
-export const useRegister = () => {
+export const useCreateOrders = () => {
   return useMutation({
     mutationFn: (order: Order) => createOrder(order),
     onError: (error) => {
       console.error("Error al crear pedido:", error);
     }
+  });
+};
+
+export const useGetOrders = () => {
+  return useQuery({
+    queryKey: ['pedidos'],
+    queryFn: getOrders,
   });
 };
