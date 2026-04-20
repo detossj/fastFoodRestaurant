5cea6ec9195abfefa86c72870a41585802ac9579
detossj <jorgerubilarssj@gmail.com>
2026-04-20 15:26:36 -0400

    Se agregaron nuevas interfaces en order.types.ts

--- src/types/order.types.ts
+++ src/types/order.types.ts
@@ -6,6 +6,7 @@ export interface OrderItem {
 }
 
 export interface Order {
+
     payment_method: string;
     total: number;
     subtotal: number;
@@ -13,4 +14,33 @@ export interface Order {
     delivery_type: string;
     delivery_address: string;
     items: OrderItem[]; 
+}
+
+export interface OrderResponse {
+    id: number;
+    payment_method: string;
+    total: number;
+    subtotal: number;
+    shipping_cost: number;
+    delivery_type: string;
+    delivery_address: string;
+    items: OrderItem[]; 
+    created_at: string;
+    order_items: OrderItem[]; 
+    order_promotion_items: OrderItem[];
+    status: string;
+}
+
+export interface ItemDetail {
+    name: string;
+    image_url: string;
+}
+
+export interface OrderItemResponse {
+    id: number;
+    quantity: number;
+    price: number;
+    cartType?: string;
+    product?: ItemDetail;  
+    promotion?: ItemDetail;
 }
