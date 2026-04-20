799da3572e83d0faf20827077d8b9a7d319dafb7
detossj <jorgerubilarssj@gmail.com>
2026-04-20 15:34:35 -0400

    Se creo la page Pedidos.tsx y se agregaron los componentes OrderSectionHeader y OrderList

---
+++ src/pages/Pedidos.tsx
@@ -0,0 +1,13 @@
+import OrderList from "@/components/orders/OrderList"
+import OrderSectionHeader from "@/components/orders/OrderSectionHeader"
+
+const Pedidos = () => {
+  return (
+    <>
+        <OrderSectionHeader/>
+        <OrderList/>
+    </>
+  )
+}
+
+export default Pedidos
