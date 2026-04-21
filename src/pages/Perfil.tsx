68c25da79d51ca71344b76f5789ee5616504f73d
detossj <jorgerubilarssj@gmail.com>
2026-04-21 12:11:22 -0400

    Se creo la page Perfil.tsx y se agregaron los componentes ProfileSectionHeader y ProfileForm

---
+++ src/pages/Perfil.tsx
@@ -0,0 +1,13 @@
+import ProfileForm from "@/components/profile/ProfileForm"
+import ProfileSectionHeader from "@/components/profile/ProfileSectionHeader"
+
+const Perfil = () => {
+  return (
+    <>
+        <ProfileSectionHeader/>
+        <ProfileForm/>
+    </>
+  )
+}
+
+export default Perfil
