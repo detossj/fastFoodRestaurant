ec6d166397b78970235d5c1be6abfd6bfdb1caf8
deto <jorgerubilarssj@gmail.com>
2026-04-22 16:56:58 -0400

    Se agrego beforeLoad para revisar si esta logeado en la ruta _public.pedidos.tsx

--- src/routes/_public.pedidos.tsx
+++ src/routes/_public.pedidos.tsx
@@ -1,8 +1,24 @@
-
 import Pedidos from '@/pages/Pedidos'
-import { createFileRoute } from '@tanstack/react-router'
-
+import { useAuthStore } from '@/stores/useAuthStore'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_public/pedidos')({
+  beforeLoad: () => {
+    const { token, rol } = useAuthStore.getState()
+
+    if (!token) {
+      throw redirect({ 
+        to: '/login', 
+        replace: true 
+      })
+    }
+
+    if (rol === 'admin') {
+      throw redirect({ 
+        to: '/admin' as any, 
+        replace: true 
+      })
+    }
+  },
   component: () => <Pedidos />
 })
